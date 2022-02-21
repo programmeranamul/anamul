3069b1ff247d7b5a17238b48f494307420af6560
programmeranamul <programmer.anamul@gmail.com>
2022-02-21 23:15:22 +0600

    change hosting

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import HomePage from "./Components/HomePage/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Menu from "./Components/MenuBar/Menu";
+import About from "./Components/AboutPage/About";
+import Footer from "./Components/ShearedFolder/Footer/Footer";
+import PortfolioPage from "./Components/PortfolioPage/PortfolioPage";
+import ArticelPage from "./Components/ArticelPage/ArticelPage";
+import ContactPage from "./Components/ContactPage/ContactPage";
+import MouseParticles from "react-mouse-particles";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Menu />
+
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/aboutMe">
+            <About />
+          </Route>
+          <Route path="/portfolio">
+            <PortfolioPage />
+          </Route>
+          <Route path="/blog">
+            <ArticelPage />
+          </Route>
+          <Route path="/contact">
+            <ContactPage />
+          </Route>
+        </Switch>
+        <Footer />
+        <MouseParticles
+          g={1}
+          num={6}
+          color="random"
+          cull="stats,image-wrapper"
+          level={6}
+        />
+        <MessengerCustomerChat
+          pageId="106794868255134"
+          appId="620513202238912"          
+        />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MenuBar/Menu.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Nav, Navbar } from "react-bootstrap";
+import { NavLink, Link } from "react-router-dom";
+import "./Menu.css";
+
+const Menu = () => {
+ 
+  
+
+  return (
+    <header className="mt-3 container-fluid">
+      <Navbar expand="md" className={`navbar-dark px-0`}>
+        <Navbar.Brand as={Link} to="/" className="logo light__white__color">
+          Anamul
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+          <Nav.Link activeClassName="active" as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link activeClassName="active" as={NavLink} to="/aboutMe">
+              About
+            </Nav.Link>
+            <Nav.Link activeClassName="active" as={NavLink} to="portfolio">
+              Portfolio
+            </Nav.Link>
+            <Nav.Link activeClassName="active" as={NavLink} to="blog">
+              Blog
+            </Nav.Link>
+            <Nav.Link activeClassName="active" as={NavLink} to="contact">
+              Contact
+            </Nav.Link>
+              <Button variant="outline-success">Resume</Button>           
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Menu;
